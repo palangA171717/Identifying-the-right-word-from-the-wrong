d136fc1e33c80bb9eab35d22ad517402ab7773ed
palangA171717 <115107665+palangA171717@users.noreply.github.com>
2022-10-05 22:12:41 +0330

    Add files via upload

---
+++ Identifying the right word from the wrong.py
@@ -0,0 +1,28 @@
+
+from tkinter import *
+from tkinter import Entry
+def a():
+    Q=e.get()
+    m=str(Q).lower()
+
+    nnnnnnnnnnnnnnnnnn="wrong"
+    n=open('h:/words.txt','r')
+    nn=(n.readlines())
+    nnnnn=len(nn)
+    for i in range((-1),nnnnn):
+     nn[i]=nn[i].lower() 
+    if((m+'\n') in nn):
+        nnnnnnnnnnnnnnnnnn="right"
+ 
+    ww=Toplevel()
+    Label(ww,text=str(nnnnnnnnnnnnnnnnnn)).pack()
+    
+    n.close()
+
+W=Tk()
+W.title("Identifying the right word from the wrong")
+#W.geometry('450x300')
+e=Entry(W,width=65)
+e.pack()
+b=Button(W,width=20,command=a).pack()
+W.mainloop()
